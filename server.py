76bd4e148957144796059fd0da9cad52c72712b3
Edward Liang <ed.l.1324@gmail.com>
2018-03-07 13:48:16 -0500

    Docstrings (#4)
    * Added testing
    
    * Fixed return bug in server
    
    * Fixed bad math in distance calculation in testing
    
    * Added client.py
    
    * Updated docstrings

--- server.py
+++ server.py
@@ -4,18 +4,24 @@ app = Flask(__name__)
 
 @app.route('/name', methods=['GET'])
 def get_name():
+    '''Returns my name (Edward Liang) as JSON'''
+
     name_dict = {'name': 'Edward Liang'}
     return jsonify(name_dict)
 
 
 @app.route('/hello/<name>', methods=['GET'])
 def hello(name):
+    '''Returns "Hello there, <:name."'''
+
     hello_dict = {'message': 'Hello there, {}'.format(name)}
     return jsonify(hello_dict)
 
 
 @app.route('/distance', methods=['POST'])
 def distance():
+    '''Returns the distance of two points from input JSON'''
+
     r = request.get_json()
     point_a = r['a']
     point_b = r['b']
@@ -32,5 +38,11 @@ def distance():
 
 
 def getDis(a, b):
+    '''Return distance between a and b
+    :param a: len 2 list (x, y)
+    :param b: len 2 list (x, y)
+    :return: Distance between a and b
+    '''
+
     dis = ((a[0] - b[0])**2 + (a[1] - b[1])**2)**0.5
     return dis
